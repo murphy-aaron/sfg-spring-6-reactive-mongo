7929a457ad7804384ff6925b4ca28c02ad1bfac8
Aaron Murphy <aamurphy98@gmail.com>
2024-12-17 12:08:12 -0500

    Initial commit

---
+++ src/main/java/dev/murphyad/reactivemongo/SfgSpring6ReactiveMongoApplication.java
@@ -0,0 +1,13 @@
+package dev.murphyad.reactivemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpring6ReactiveMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgSpring6ReactiveMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/murphyad/reactivemongo/SfgSpring6ReactiveMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.murphyad.reactivemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgSpring6ReactiveMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
